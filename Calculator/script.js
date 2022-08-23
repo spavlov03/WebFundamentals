89e256762cc2a1c3835ce24d3dd6c33bce3d7adb
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-08-23 12:03:31 -0700

    Calculator Update

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,24 +1,61 @@
 var displayDiv = document.querySelector("#display");
+// displayDiv.value = displayDiv.innerText;
 displayDiv.innerText = "";
-var math = ""; 
-var opertator ="";
+var operator = "";
+var result = 0;
+var num1 = 0; 
+var num2 = 0; 
 
 function press(element) {
     displayDiv.innerText += element;
-    math += element;
-    // console.log(math);
+    displayDiv.value = displayDiv.innerText; 
+    if (operator == "") {
+        num1 = displayDiv.value;
+    } else {
+        num2 = displayDiv.value;
+    }
+    // console.log(operator);
+    // console.log("This is num1 " + num1);
+    // console.log("This is num2 " + num2);
 }
 
 function clr() { 
     displayDiv.innerText = "";
-    math = "";
+    num1 = 0; 
+    num2 = 0; 
+    operator = ""; 
 }
 
 function setOP(element) {
-    math += element;
+    operator = element;
     displayDiv.innerText = "";
-    // console.log(math);
+    // console.log("This is the operator " + operator);
+    
 }
 function calculate() { 
-    displayDiv.innerText = parseInt(eval(math));
+    if (operator == "+" ) {
+        result = parseFloat(num1) + parseFloat(num2); 
+        displayDiv.value= result; 
+        displayDiv.innerText = displayDiv.value; 
+        num1 = result; 
+        // console.log("Result with + " + displayDiv.value);
+    } else if (operator == "-") {
+        result = parseFloat(num1) - parseFloat(num2); 
+        displayDiv.value= result; 
+        displayDiv.innerText = displayDiv.value; 
+        num1 = result; 
+        // console.log("Result with - " + displayDiv.value);
+    } else if (operator == "/") {
+        result = parseFloat(num1) / parseFloat(num2); 
+        displayDiv.value= result; 
+        displayDiv.innerText = displayDiv.value; 
+        num1 = result; 
+        // console.log("Result with / " + displayDiv.value);
+    } else if (operator == "*") {
+        result = parseFloat(num1) * parseFloat(num2); 
+        displayDiv.value= result; 
+        displayDiv.innerText = displayDiv.value; 
+        num1 = result; 
+        // console.log("Result with * " + displayDiv.value);
+    }
 }
